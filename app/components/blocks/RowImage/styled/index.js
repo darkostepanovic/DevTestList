6681a38a0602316920ae9981f4da1ae8d4f31744
Darko Stepanovic <dare.stepanovic@gmail.com>
2019-06-11 12:33:39 +0200

    small styling fix

--- app/components/blocks/RowImage/styled/index.js
+++ app/components/blocks/RowImage/styled/index.js
@@ -5,6 +5,7 @@ export const Wrapper = styled(Animated.View)`
   position: absolute;
   top: 0;
   bottom: 0;
+  width: 100%;
   align-self: center;
   align-items: center;
   justify-content: center;
